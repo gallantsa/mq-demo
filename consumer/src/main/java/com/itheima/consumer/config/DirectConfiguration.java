057b3b9f7c2253dc6920dd35b36cfd7ce07e81d2
gallantsa <gallantsa@163.com>
2023-10-28 11:59:37 +0800

    MQ_Java客户端_声明队列和交换机的方式一

---
+++ consumer/src/main/java/com/itheima/consumer/config/DirectConfiguration.java
@@ -0,0 +1,48 @@
+package com.itheima.consumer.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DirectConfiguration {
+
+    @Bean
+    public DirectExchange directExchange() {
+        /*ExchangeBuilder.fanoutExchange("").build();*/
+        return new DirectExchange("hmall.direct");
+    }
+
+    @Bean
+    public Queue directQueue1() {
+        /*QueueBuilder.durable("ff");*/ // 持久化
+        return new Queue("direct.queue1"); // 默认持久
+    }
+
+    @Bean
+    public Binding directQueue1BindingRed(Queue directQueue1, DirectExchange directExchange) {
+        return BindingBuilder.bind(directQueue1).to(directExchange).with("red");
+    }
+
+    @Bean
+    public Binding directQueue1BindingBlue(Queue directQueue1, DirectExchange directExchange) {
+        return BindingBuilder.bind(directQueue1).to(directExchange).with("blue");
+    }
+
+    @Bean
+    public Queue directQueue2() {
+        /*QueueBuilder.durable("ff");*/ // 持久化
+        return new Queue("direct.queue2"); // 默认持久
+    }
+
+    @Bean
+    public Binding directQueue2BindingRed(Queue directQueue2, DirectExchange directExchange) {
+        return BindingBuilder.bind(directQueue2).to(directExchange).with("red");
+    }
+
+    @Bean
+    public Binding directQueue2BindingBlue(Queue directQueue2, DirectExchange directExchange) {
+        return BindingBuilder.bind(directQueue2).to(directExchange).with("yellow");
+    }
+
+}
